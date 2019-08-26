00fd42d6502f318e4154c9058455094420653373
Chizzy <chizoba.princess@gmail.com>
2019-08-26 11:35:24 -0400

    set up two components with text fields

--- code-challenge/src/App.js
+++ code-challenge/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import C1 from './C1';
+import C2 from './C2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>React Basics Coding Challenge</h1>
+        <C1 />
+        <C2 />
+      </div>
+    )
+  }
 }
 
 export default App;
