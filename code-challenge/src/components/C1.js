0834f04a92b4e2153bf1624a4275df79fa2239af
Chizzy <chizoba.princess@gmail.com>
2019-08-27 23:05:12 -0400

    add GET request to button in C1

--- code-challenge/src/components/C1.js
+++ code-challenge/src/components/C1.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import C2 from './C2';
+import axios from 'axios';
 
 class C1 extends Component {
     state = {
         C1: 'www.placeholder.com',
-        C2: ''
+        C2: '',
+        fact: '',
+        length: 0
     }
     handleClick = (event) => {
         event.preventDefault()
@@ -13,11 +16,27 @@ class C1 extends Component {
     getFromC2 = (modifiedC2) => {
         this.setState({C1 : modifiedC2})
     }
+    handleSubmit =(event) => {
+        event.preventDefault()
+        const url = ('https://cors-anywhere.herokuapp.com/' + this.state.C1)
+        axios
+            .get(url)
+            .then(response  => {
+                console.log(response.data)
+                this.setState({
+                    fact: response.data.fact,
+                    length: response.data.length
+                })
+            })
+            .catch(error => {
+                console.log("FAIL: " + error)
+            })
+    }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" name="C1" value={this.state.C1} readOnly /><br />
                     <button onClick={this.handleClick}>Send to C2</button>
                     <button type="submit">Access Address</button>
